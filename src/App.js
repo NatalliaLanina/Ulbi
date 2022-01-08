4bb5bafac454ba8e57bf65f426cacdf149831fdc
Natallia Lanina <natalielanina@yahoo.com>
2022-01-08 17:49:20 +0300

    feat: cra

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('some text');
+
+  let increment = () => {
+    setLikes(likes + 1)
+  }
+  let decrement = () => {
+    setLikes(likes - 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <h1>{likes}</h1>
+      <h2>Text: {value}</h2>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value) }
+      />
     </div>
   );
 }
