e04efc7453e21dca48fe5046cffe96b409e1040a
Natallia Lanina <natalielanina@yahoo.com>
2022-01-24 12:39:06 +0300

    feat: add search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import '../src/styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
     {id: 1, title: 'aaa', body: 'anna'},
     {id: 2, title: 'vvv', body: 'natallia'},
-    {id: 3, title: 'ccc', body: 'bobo'},
-  ])
-  const [selectedSort, setSelectedSort] = useState('')
+    {id: 3, title: 'ccc', body: 'bob'},
+  ]);
+  const [selectedSort, setSelectedSort] = useState('');
+  const [search, setSearch] = useState('');
+
+  const sortedPosts = useMemo(() => {
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }, [selectedSort, posts]);
+
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.includes(search))
+  }, [search, sortedPosts])
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -20,12 +35,16 @@ function App() {
   }
   const sortPosts = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
     <div className="app">
       <PostForm create={createPost}/>
+      <MyInput
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+        placeholder='Search'
+      />
       <div>
         <MySelect
           value={selectedSort}
@@ -38,7 +57,7 @@ function App() {
         />
       </div>
       {posts.length
-        ? <PostList removePost={removePost} posts={posts} title={'Post list 1'} key={posts.id}/>
+        ? <PostList removePost={removePost} posts={sortedAndSearchedPosts} title={'Post list 1'} key={posts.id}/>
         : <h2>No posts</h2>}
     </div>
   );
