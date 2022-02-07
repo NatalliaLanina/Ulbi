b845f638a1ac4595040c3b41acd3290367885957
Natallia Lanina <natalielanina@yahoo.com>
2022-02-07 14:31:57 +0300

    feat: add post animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Post from "./Post";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = (props) => {
   if (!props.posts.length) {
@@ -9,9 +10,18 @@ const PostList = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      {props.posts.map(post =>
-        <Post remove={props.removePost} post={post} key={post.id}/>
-      )}
+      <TransitionGroup>
+        {props.posts.map(post =>
+          <CSSTransition
+            key={post.id}
+            timeout={500}
+            classNames='post'
+          >
+            <Post remove={props.removePost} post={post}/>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+
     </div>
   );
 };
