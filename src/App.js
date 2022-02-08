157b110f914a1b77c6836a4ce1d06fcb19c75036
Natallia Lanina <natalielanina@yahoo.com>
2022-02-08 13:21:45 +0300

    feat: add usePosts

--- src/App.js
+++ src/App.js
@@ -5,36 +5,21 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'aaa', body: 'anna'},
-    {id: 2, title: 'vvv', body: 'natallia'},
-    {id: 3, title: 'ccc', body: 'bob'},
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
-  const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
-
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false)
-  }
+  };
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
-  }
+  };
 
   return (
     <div className="app">
