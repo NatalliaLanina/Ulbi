ff9f75606aba4d4e53d5093d60ec532f0c2e757b
Natallia Lanina <natalielanina@yahoo.com>
2022-02-16 12:48:34 +0300

    feat: add loader

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
   useEffect(() => {
     fetchPosts();
@@ -26,20 +28,23 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   };
   const fetchPosts = async () => {
+    setIsPostsLoading(true);
     const posts = await PostService.getAll()
     setPosts(posts);
+    setIsPostsLoading(false);
   }
 
   return (
     <div className="app">
-      <MyButton onClick={() => setModal(true)}>
-        Create post
-      </MyButton>
+      <MyButton onClick={() => setModal(true)}>Create post</MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost}/>
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter}/>
-      <PostList removePost={removePost} posts={sortedAndSearchedPosts} title={'Post list 1'} key={posts.id}/>
+      {isPostsLoading
+        ? <Loader/>
+        : <PostList removePost={removePost} posts={sortedAndSearchedPosts} title={'Post list 1'} key={posts.id}/>
+      }
     </div>
   )
 }
